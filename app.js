669a2c8683bc5e3c0c0fd37e411fc84ea38c5ed1
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-05-11 13:53:36 -0700

    Add persistence and JSON parsing

--- app.js
+++ app.js
@@ -6,16 +6,44 @@ var React = require('react');
 var TreePie = require('./TreePie');
 var OutlineParser = require('./OutlineParser');
 
+function format(string) {
+  try {
+    return JSON.stringify(JSON.parse(string), null, 2);
+  } catch(e) {
+    return string;
+  }
+}
+
+function parse(string) {
+  if (string[0] === '[') {
+    try {
+      return JSON.parse(string);
+    } catch (e) {
+      console.log(e);
+      return undefined;
+    }
+  } else {
+    return OutlineParser(string);
+  }
+}
+
 var InputField = React.createClass({
+  sendData: function(string) {
+    var parsed = parse(string);
+    if (parsed) this.props.onChange(parsed);
+  },
   getInitialState: function() {
-    return {string: 'Body\nMind\nSoul\n A\n B\n C'};
+    var string = localStorage.treepieDemo || 'Body\nMind\nSoul\n A\n B\n C';
+    string = format(string);
+    return {string: string};
   },
   componentDidMount: function() {
-    this.props.onChange(OutlineParser(this.state.string));
+    this.sendData(this.state.string);
   },
   onChange: function(e) {
     var string = this.refs.textarea.getDOMNode().value;
-    this.props.onChange(OutlineParser(string));
+    localStorage.treepieDemo = string;
+    this.sendData(string);
   },
   render: function() {
     return <textarea onChange={this.onChange} ref="textarea">{this.state.string}</textarea>;
